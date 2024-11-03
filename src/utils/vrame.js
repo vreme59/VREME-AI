d119269db7efcc9a2bc77524f9ef4b47be794f37
vreme59 <vreme592@gmail.com>
2024-11-03 12:12:46 +0900

    Add files via upload

---
+++ src/utils/vrame.js
@@ -0,0 +1,15 @@
+// first.js
+var dataprompt = {
+  main  : "[System Note: Namamu adalah 'VReme AI'. Tugas mu adalah menjawab dan membantu pertanyaan dari User.]",
+  nsfw : "#888",
+  side  : "#369"
+};
+
+
+module.exports = dataprompt;
+
+var list = {
+  MainPrompt: add,
+  history: add,
+  Chatuser: add,
+}
